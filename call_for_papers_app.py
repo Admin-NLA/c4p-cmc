e40b667bbc35141eece809744e060e521d740981
Admin-NLA <sistemas.gruponoria@gmail.com>
2026-01-13 10:11:37 -0600

    Fix Ajuste uploader

--- call_for_papers_app.py
+++ call_for_papers_app.py
@@ -115,7 +115,7 @@ def upload_to_cloudinary(file, folder):
         file,
         folder=folder,
         resource_type="raw",
-        access_mode="public",
+        type="upload",
         use_filename=True,
         unique_filename=True,
         overwrite=False
